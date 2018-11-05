10de4346db7cde701d36b52c9f7170d5567fe30a
andrew cochrane <iamandrewcochrane@gmail.com>
2018-11-05 13:41:40 +0000

    update project hotel

---
+++ models/hotel.js
@@ -0,0 +1,13 @@
+class Hotel {
+    constructor(name, city){
+        this.name = name
+        this.city = city 
+        this.reviews = []
+    }
+
+    reviewCount(){
+        return 0
+    }
+
+}
+module.exports = Hotel
---
+++ test/hotel.test.js
@@ -0,0 +1,37 @@
+const chai = require('chai');
+const expect = chai.expect; // is the function for expect
+
+var Hotel = require('../models/hotel') //required hotel class
+
+describe('Hotel', function () {
+    describe('constructor', function () {
+        it('sets the name attribute from its inputs', function () {
+            var hotel = new Hotel("Hilton Metropole", "London")
+            expect(hotel.name).to.equal('Hilton Metropole')
+
+        })
+
+        it('sets the city attribute from its inputs', function () {
+            var hotel = new Hotel("Hilton Metropole", "London")
+            expect(hotel.city).to.equal('London')
+        })
+
+        it('sets the review attribute to an empty array', function () {
+            var hotel = new Hotel("Hilton Metropole", "London")
+            expect(hotel.reviews).to.deep.equal([]) //adding to.deep to equal when it comes to Arrays
+        })
+
+    })
+
+    //describe block
+
+    describe('reviewCount', function(){
+        it('returns 0 when there are no reviews', function(){
+            var hotel = new Hotel("Hilton Metropole", "London")
+            var result = hotel.reviewCount() //to verify method
+            expect(result).to.equal(0) //verifying
+
+        })
+    })
+
+});
